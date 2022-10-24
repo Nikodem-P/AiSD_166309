56c18fae8106d2731585a3f68c22bb14ddbf198c
Nikodem <97964965+Nikodem-P@users.noreply.github.com>
2022-10-24 10:42:02 +0200

    10:41

--- LAB3/main.py
+++ LAB3/main.py
@@ -1,5 +1,6 @@
 from linkedlist import Node, LinkedList
 from stack import Stack
+from queue import Queue
 
 stos = Stack()
 print(f'Dlugosc: {len(stos)}')
@@ -18,3 +19,29 @@ print(f'Stos: {stos}')
 print(f'Dlugosc: {len(stos)}')
 print(f'Pop: {stos.pop()}')
 print(f'Pop: {stos.pop()}')
+
+print("================================")
+
+bufor = Queue()
+print(len(bufor))
+bufor.enqueue(11)
+print(len(bufor))
+print(bufor.peek())
+print(bufor.dequeue())
+print(len(bufor))
+
+print("")
+bufor.enqueue(12)
+print(bufor)
+print(bufor.dequeue())
+print(bufor)
+
+print("")
+bufor.enqueue(13)
+bufor.enqueue(14)
+print(bufor)
+print(bufor.peek())
+print(len(bufor))
+print(bufor.dequeue())
+print(bufor.dequeue())
+print(len(bufor))
