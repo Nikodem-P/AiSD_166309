605628a06ff815a8b76700392cea6067e9f58e62
Nikodem <97964965+Nikodem-P@users.noreply.github.com>
2022-10-24 10:40:50 +0200

    2 rebuilt print

--- LAB3/stack.py
+++ LAB3/stack.py
@@ -12,18 +12,19 @@ class Stack:
         return len(self._storage)
 
     def __str__(self):
+        str_: str = '\n'
         temp: list[Any] = []
         for i in range(len(self._storage)):
             temp.append(self._storage.node(i).value)
         i = len(temp) - 1
         while i >= 0:
-            print(temp[i])
+            str_ += f'{temp[i]}\n'
             i -= 1
-        return ''
+        return str_
 
     def push(self, element: Any) -> None:
         self._storage.append(element)
 
     def pop(self):
-        return self._storage.pop()
+        return self._storage.pop_back()
 
