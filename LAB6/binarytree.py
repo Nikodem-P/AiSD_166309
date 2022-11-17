7ade5b083d2f64689d14ed88d958587c43d18f7a
Nikodem <97964965+Nikodem-P@users.noreply.github.com>
2022-11-17 11:07:32 +0100

    3: BST

--- LAB6/binarytree.py
+++ LAB6/binarytree.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable
+from typing import List, Any, Callable
 import graphviz
 
 
@@ -55,11 +55,16 @@ class BinaryNode:
         if self.right_child is not None:
             dot.edge(str(self), str(self.right_child))
 
+    def min(self) -> 'BinaryNode':
+        if self.left_child is None:
+            return self
+        return self.left_child.min()
+
 
 class BinaryTree:
     root: BinaryNode
 
-    def __init__(self, root_val):
+    def __init__(self, root_val: Any):
         self.root = BinaryNode(root_val)
 
     def traverse_in_order(self, visit: Callable[[Any], None]) -> None:
@@ -76,3 +81,53 @@ class BinaryTree:
         self.traverse_in_order(lambda x: dot.node(str(x), str(x)))
         self.traverse_in_order(lambda x: x.add_edges(dot))
         print(dot.source)
+
+
+class BinarySearchTree(BinaryTree):
+
+    def _insert(self, node: BinaryNode, value: Any) -> BinaryNode:
+        if value < node.value:
+            if node.left_child is None:
+                node.left_child = BinaryNode(value)
+                return node.left_child
+            return self._insert(node.left_child, value)
+
+        if node.right_child is None:
+            node.right_child = BinaryNode(value)
+            return node.right_child
+        return self._insert(node.right_child, value)
+
+    def insert(self, value: Any) -> None:
+        self._insert(self.root, value)
+
+    def insert_list(self, list_: List[Any]):
+        for i in list_:
+            self.insert(i)
+
+    def contains(self, value: Any) -> bool:
+        node = self.root
+        while node.left_child is not None or node.right_child is not None:
+            if node.value == value:
+                return True
+            elif node.value > value:
+                node = node.left_child
+            else:
+                node = node.right_child
+        return node.value == value
+
+    def _remove(self, node: BinaryNode, value: Any):
+        if value == node.value:
+            if node.left_child is None:
+                return node.right_child
+            if node.right_child is None:
+                return node.left_child
+            node.value = node.right_child.min().value
+            node.right_child = self._remove(node.right_child, node.right_child.value)
+        if value < node.value:
+            node.left_child = self._remove(node.left_child, value)
+        if value > node.value:
+            node.right_child = self._remove(node.right_child, value)
+        return node
+
+    def remove(self, value: Any):
+        self._remove(self.root, value)
--- LAB6/main.py
+++ LAB6/main.py
@@ -1,18 +1,30 @@
 from binarytree import *
 
-tree = BinaryTree(10)
-a = tree.root
-a.add_left_child(9)
-a.add_right_child(2)
-a.left_child.add_left_child(1)
-a.left_child.add_right_child(3)
-a.right_child.add_left_child(4)
-a.right_child.add_right_child(6)
-tree.show()
-assert tree.root.value == 10
+# tree = BinaryTree(10)
+# a = tree.root
+# a.add_left_child(9)
+# a.add_right_child(2)
+# a.left_child.add_left_child(1)
+# a.left_child.add_right_child(3)
+# a.right_child.add_left_child(4)
+# a.right_child.add_right_child(6)
+# tree.show()
+# assert tree.root.value == 10
+#
+# assert tree.root.right_child.value == 2
+# assert tree.root.right_child.is_leaf() is False
+#
+# assert tree.root.left_child.left_child.value == 1
+# assert tree.root.left_child.left_child.is_leaf() is True
 
-assert tree.root.right_child.value == 2
-assert tree.root.right_child.is_leaf() is False
+def main():
+    tree = BinarySearchTree(8)
+    tree.insert_list([3, 1, 6, 4, 7, 10, 14, 13])
+    print(tree.contains(6))
+    tree.show()
+    tree.remove(8)
+    tree.show()
 
-assert tree.root.left_child.left_child.value == 1
-assert tree.root.left_child.left_child.is_leaf() is True
+
+if __name__ == '__main__':
+    main()
